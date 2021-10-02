89170d1c38e0bec5202e4950fea88ecb0a5edf2f
yellow-jellyfish <90250541+yellow-jellyfish@users.noreply.github.com>
2021-10-02 11:05:55 +0200

    Add files via upload

---
+++ model_trainer.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import keras
+import logging
+import os
+from sklearn.ensemble import RandomForestClassifier
+from flask import jsonify
+from google.cloud import storage
+from sklearn import ensemble 
+
+def train_titanic(train_data):
+    y = train_data["Survived"]
+    features = ["Pclass", "Sex", "SibSp", "Parch"]
+    X = pd.get_dummies(train_data[features])
+    X_test = pd.get_dummies(test_data[features])
+
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+    model.fit(X, y)
+    predictions = model.predict(X_test)#
+    
+    scores = model.evaluate(X, y)
+    print(model.metrics_names)
+    text_out = {
+        "accuracy:": scores[1],
+        "loss": scores[0],}
+    
+    output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+    output.to_csv('submission.csv', index=False)
+    print("Your submission was successfully saved!")
+    #this is part from lab, saving the model and returing the result 
+    model_repo = os.environ['MODEL_REPO']
+        if model_repo:
+            file_path = os.path.join(model_repo, "model.h5")
+            model.save(file_path)
+            logging.info("Saved the model to the location : " + model_repo)
+            return jsonify(text_out), 200
+        else:
+            model.save("model.h5")
+            return jsonify({'message': 'The model was saved locally.'}), 200
+
+
+if __name__ == '__main__':
+    train_titanic()
